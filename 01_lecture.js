a3c85bf7892c14c8d847ccd823bac995f95f368c
Shreyansh <shreyanshmarshal2@gmail.com>
2023-11-25 22:24:07 +0530

    Const let and var

---
+++ 01_lecture.js
@@ -0,0 +1,14 @@
+const accountId  = 144553
+let accountEmail = "shreyansh@gmail.com" //can be assigned a value but can't declared again
+var accountPassword = "shreyansh" //can be re assigned a value and can be declared again
+accountCity = "Greater noida"
+
+// accountId = 2 -> This is not possible bcz we are assiging to a constant variable
+
+accountEmail = "badal@gmail.com"
+accountPassword = "badal"
+accountCity = "Bihar"
+var  accountPassword = "shreyanshsingh"
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
