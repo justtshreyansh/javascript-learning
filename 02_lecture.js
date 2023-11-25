ed3edcbdc8ec509b22666db8974955d8d3589b59
Shreyansh <shreyanshmarshal2@gmail.com>
2023-11-25 22:37:41 +0530

    variables and data types

---
+++ 02_lecture.js
@@ -0,0 +1,12 @@
+"use strict";  //treat all code as newer JS version
+
+// alert(3+2) //We are using node js and not browser
+
+//Code readability should be high -> Very important tips
+
+let name = "shreyansh" //string
+let age = 19 //integer number
+let isLoggedIn = true //boolean value
+let state = null;
+//null => Standalone value
+//undefined => declared the variable but not assigned the value
