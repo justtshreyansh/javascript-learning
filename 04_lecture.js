cb7f3c5e2e6bc1940826a4f3e54b6986c26ddd52
Shreyansh <shreyanshmarshal2@gmail.com>
2023-11-30 19:52:34 +0530

    why string to number is confusing

---
+++ 04_lecture.js
@@ -0,0 +1,16 @@
+let str1 = "shreyansh"
+let str2 = " singh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log(1+2+"3");
+console.log("1"+2+2);
+
+
+let gameCounter =100;
+
+++gameCounter;
+console.log(gameCounter);
+
