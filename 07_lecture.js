27fad4a05fe94d3a5706d77f6cd57b2485e0823e
Shreyansh <shreyanshmarshal2@gmail.com>
2023-12-01 20:42:58 +0530

    String in javascript

---
+++ 07_lecture.js
@@ -0,0 +1,17 @@
+//String interpolation
+
+const name = "Shreyansh singh"
+const age = 19;
+
+console.log(`My name is ${name} and current age is ${age}`); //String interpolation
+
+const gameName = new String("justtshreyansh");
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const username = "Shreyansh_singh_king";
+console.log(username.split('_'));
