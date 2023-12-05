f4819b7e128cc93d81a35dc7e43c04d0a36d93ac
Shreyansh <shreyanshmarshal2@gmail.com>
2023-12-05 10:25:29 +0530

    Date and Time

---
+++ dateAndTime.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(typeof myDate);
