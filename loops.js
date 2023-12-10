decd6df33d3654ab81cf7b9ddf5332545a5e8506
Shreyansh <shreyanshmarshal2@gmail.com>
2023-12-10 23:06:26 +0530

    For,while and do while loops in javascript

---
+++ loops.js
@@ -0,0 +1,22 @@
+//for loops
+let n = 10;
+for(let i=0;i<n;i++){
+    console.log(i);
+}
+
+//while loop
+
+while(n!=0){
+    console.log(n);
+    n--;
+}
+
+//do while loops
+
+
+let numm = 10;
+
+do{
+    console.log(numm);
+}
+while(n==11);
