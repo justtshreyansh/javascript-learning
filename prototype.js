1f3afd9457d01e8de7e05122c375770aa45c623c
Shreyansh <shreyanshmarshal2@gmail.com>
2023-12-24 08:36:30 +0530

    Prototype

---
+++ prototype.js
@@ -0,0 +1,25 @@
+function multiplyBy5(num){
+    return num*5
+}
+//function is a object as well as function
+multiplyBy5.power = 2;
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.power)
+console.log(multiplyBy5.prototype)
+
+function createUser(username,score){
+    this.username = username
+    this.score = score
+
+}
+
+createUser.prototype.increment = function(){
+    this.score++;
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai",25)
+const tea =  new createUser("tea",250)
+chai.printMe()
